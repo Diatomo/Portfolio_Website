759a57e9e737b919736f0fc28fc79a56e00055f5
Charles Stevenson <charles.stevenson222@gmail.com>
2024-06-27 22:51:15 -0400

    Added Minify. Still looking into bundling.

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_static_digest import FlaskStaticDigest
-
+from flask_minify import minify
 
 flask_static_digest = FlaskStaticDigest()
 
 app = Flask(__name__)
 flask_static_digest.init_app(app)
+minify(app=app, html=True, js=True, cssless=True)
 
 
 
